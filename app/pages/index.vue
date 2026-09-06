06a5384668059e24d398c2608bbbcfd0784ecacd
Santiago Cézar <santiagocezar2013@gmail.com>
2026-09-06 11:32:26 -0300

    usar nested routes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,42 @@
 <template>
-    <ul>
-        <li><a href="/sports">Deportes</a></li>
-        <li><a href="/teams">Equipos</a></li>
-        <li><a href="/players">Jugadores</a></li>
-    </ul>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+                <img src="~/assets/img/67.png" alt="" />
+            </a>
+            <!--
+            <a
+                role="button"
+                class="navbar-burger"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+            >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a> -->
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="/sports"> Deportes </a>
+                <a class="navbar-item" href="/teams"> Equipos </a>
+                <a class="navbar-item" href="/players"> Jugadores </a>
+            </div>
+            <!--
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light"> Log in </a>
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </nav>
+    <NuxtPage />
 </template>
